42909673eaaa667d45f37c76a8b32cda67ce05fa
Oscar <oscarmdial@gmail.com>
2022-03-25 12:07:19 +0000

    navbar and newPodcast feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import PodcastNew from './NewPodcast';
 
 import '../styles/style.scss';
 
@@ -7,7 +9,8 @@ const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="/" element={<h1>Hello world</h1>} />
+      <Route path='/' element={<h1>s world</h1>} />
+      <Route path='createpodcast' element={<PodcastNew />} />
     </Routes>
   </BrowserRouter>
 );
